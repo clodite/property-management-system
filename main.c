620501544654fd9c01c4918834315953dac619e0
wosikeaixiaonanniang <3146715671@qq.com>
2025-03-13 19:04:42 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
-#include "function.h"
-#include "header.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "nb.h"
 
-int main()
+
+
+
+
+struct room* createNode(int data)//创建新房间
 {
+    struct room* newroom = (struct room*)malloc(sizeof(struct room));
+    if (!newroom) {
+        printf("内存分配失败\n");
+        exit(1);
+    }
+    newroom->next = NULL;
+    return newroom;
+}
+int main() 
+{
+   
+
+   
+
     return 0;
 }
