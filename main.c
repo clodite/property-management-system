66551b2a950335514059c45dbbf8ca76f84dba0f
wosikeaixiaonanniang <3146715671@qq.com>
2025-03-13 19:05:12 +0800

    Update main.c

--- main.c
+++ main.c
@@ -6,16 +6,7 @@
 
 
 
-struct room* createNode(int data)//创建新房间
-{
-    struct room* newroom = (struct room*)malloc(sizeof(struct room));
-    if (!newroom) {
-        printf("内存分配失败\n");
-        exit(1);
-    }
-    newroom->next = NULL;
-    return newroom;
-}
+
 int main() 
 {
    
