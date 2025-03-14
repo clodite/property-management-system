7932e11f015fd97089827427108cad1312df8222
wosikeaixiaonanniang <3146715671@qq.com>
2025-03-14 17:25:59 +0800

    Update struct.h

--- struct.h
+++ struct.h
@@ -20,22 +20,18 @@ struct user
     char password[30]; // 密码
     char email[30];    // 邮箱
     char name[30];     // 姓名
+    char name[30];     // 名字
+    int age;           // 年龄
+    char call[20];     // 电话
+    int room;          // 入住房间号
+    int arrive_time;   // 入住时间 按时间戳计算
+    int time_live;     // 入住时长 在办理入住时计算
+    int leave_time;    // 退房时间 未退房记作-1
+    int card;          // 客户等级
+    // char booktime[30];  // 预定时间
     struct user *next; // 指向下一个节点的指针
 };
 
-struct custom
-{
-    char name[30];   // 名字
-    int age;         // 年龄
-    char call[20];   // 电话
-    int room;        // 入住房间号
-    int arrive_time; // 入住时间 按时间戳计算
-    int time_live;   // 入住时长 在办理入住时计算
-    int leave_time;  // 退房时间 未退房记作-1
-    int card;        // 客户等级
-    // char booktime[30];  // 预定时间
-    struct custom *next; // 指向下一个节点的指针
-};
 
 struct room
 {
@@ -65,7 +61,7 @@ struct room *create_room(int num, int type, int price) // 创建新房间
     return newroom;
 }
 
-struct user *create_user(int type, char ID[30], char password[30], char email[30], char name[30]) // 创建账户
+struct user *create_user(int type, char ID[30], char password[30], char email[30], char name[30],char name[30], int age, char call[20], int room, int arrive_time, int time_live, int leave_time, int card) // 创建账户
 {
     struct user *newuser = (struct user *)malloc(sizeof(struct user));
     if (!newuser)
@@ -80,17 +76,6 @@ struct user *create_user(int type, char ID[30], char password[30], char email[30
     newuser->email[30] = email[30];
     newuser->name[30] = name[30];
     strcpy(newuser->card, "none");
-    return newuser;
-}
-
-struct custom *create_custom(char name[30], int age, char call[20], int room, int arrive_time, int time_live, int leave_time, int card) // 创建客人
-{
-    struct custom *newcustom = (struct custom *)malloc(sizeof(struct custom));
-    if (!newcustom)
-    {
-        printf("内存分配失败\n");
-        exit(1);
-    }
     strcpy(newcustom->name, name);
     newcustom->age = age;
     strcpy(newcustom->call, call);
@@ -100,5 +85,7 @@ struct custom *create_custom(char name[30], int age, char call[20], int room, in
     newcustom->leave_time = leave_time;
     newcustom->card = card;
     newcustom->next = NULL;
-    return newcustom;
+    return newuser;
 }
+
+
