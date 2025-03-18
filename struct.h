4bd4fdbbc321c553bb15e5cc31056ad6cdb1627d
wosikeaixiaonanniang <3146715671@qq.com>
2025-03-18 16:50:24 +0800

    Update struct.h

--- struct.h
+++ struct.h
@@ -8,7 +8,6 @@ struct time
     int day;
     int hour;
 };
-
 struct room
 {
     int num;   // 房号
@@ -17,25 +16,27 @@ struct room
     int size;  // 客房面积
     // int state;          // 客房状态 0=>空闲 1=>已预定 2=>已入住
     // struct user *custom;// 入住客人的数据
-    struct room *next; // 指向下一个节点的指针
+    struct room* next; // 指向下一个节点的指针
 };
+
 
 struct user
 {
     char ID[30];       // id
-    char call[20];     // 电话
     char password[30]; // 密码
-    char email[30];    // 邮箱
-    char name[30];     // 姓名
-    char IDnum[30];    // 身份证号
-    int age;           // 年龄
     int type;          // 用户类型 0=>管理员 1=>用户 2=>客人
+    char name[30];     // 真实姓名
+    int age;           // 真实年龄
+    char call[20];     // 用户电话
+    int card;          // 客户等级
+    struct user* next; // 指向下一个节点的指针
+};
+struct live_record
+{
+    char ID[30];       // id
     int room;          // 入住房间号
     int arrive_time;   // 入住时间 按时间戳计算
     int time_live;     // 入住时长 在办理入住时计算
     int leave_time;    // 退房时间 未退房记作-1
-    int card;          // 客户等级
-    // char booktime[30];  // 预定时间
-    struct user *next; // 指向下一个节点的指针
 };
 
