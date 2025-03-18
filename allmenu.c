8b2a2606c0bc9d6e06cc9ba4f1a7cdc2dfdc0aa5
wosikeaixiaonanniang <3146715671@qq.com>
2025-03-18 16:45:08 +0800

    Add files via upload

---
+++ allmenu.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "struct.h"
+#include "header.h"
+#include "function.h"
+
+//这里放三种账号登录后给出的操作界面
+void menu_manager(char ID[30], char password[30], int type, char name[30], int age, char call[20], int card)
+{
+	;
+}
+void menu_front(char ID[30], char password[30], int type, char name[30], int age, char call[20], int card)
+{
+	;
+}
+void menu_custom(char ID[30], char password[30], int type, char name[30], int age, char call[20], int card)
+{
+	;
+}
