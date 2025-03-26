2fcbef7daf01c857d70f031be688cf65edc595a2
szxzip-manjaro <64679700+szxzip@users.noreply.github.com>
2025-03-26 23:28:36 +0800

    chore: 'C'ontinue

--- function.h
+++ function.h
@@ -15,10 +15,10 @@ void menu_custom(char ID[30], char password[30], int type, char name[30], int ag
 
 // C
 int cancel_my_booking(long long my_book_id);         // 取消自己的预约
-struct strbook* view_my_booking(char my_id[30]);     // 查询某人所有预约
-struct strbook* view_all_bookings();                 // 查询所有预约
+struct strbook* view_my_bookings(char my_id[30]);    // 查询某人所有预约
+struct strbook* view_all_bookings(long long book_id, int time, int roomtype, int last, char id[30], int status);   // 查询所有预约
 // count_all_bookings(); // 统计所有预约
-// sort_all_bookings(); // 排序所有预约
+// sort_all_bookings(int order); // 排序所有预约
 
 struct user* load_users(const char* users);  //将用户数据全部读取存入链表，返回首地址
 void write_users(const char* users, struct user* head); //将链表数据写入users文件中 
