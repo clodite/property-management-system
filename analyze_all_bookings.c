ce958483486b9eb77304da2419dbac12ac567e0a
szxzip-manjaro <64679700+szxzip@users.noreply.github.com>
2025-03-27 12:44:09 +0800

    chore: 'C'ontinue

--- analyze_all_bookings.c
+++ analyze_all_bookings.c
@@ -17,7 +17,7 @@ struct strbook* view_all_bookings(long long book_id, int time, int roomtype, int
     struct strbook* head = NULL;
     struct strbook* tail = NULL;
     char line[256];
-    int bugs = 0; // 没用
+    int bug = 0; // 没用
 
     while (fgets(line, sizeof(line), book_record))
     {
@@ -25,8 +25,47 @@ struct strbook* view_all_bookings(long long book_id, int time, int roomtype, int
         int current_time, current_roomtype, current_last, current_status;
         char current_id [30];
 
-        if ()
+        if (sscanf(line, "%lld %d %d %d %c %d", &current_book_id, &current_time, &current_roomtype, &current_last, current_id, &current_status) != 6)
+        {
+            bug++;
+            continue;
+        }
+
+        if ((current_book_id != book_id && book_id != -1) || (current_time != time && time != -1) || (current_roomtype != roomtype && roomtype != -1) || (current_last != last && last != -1) || (strcmp(current_id, id) && strcmp(id, "-1")) || (current_status != status && status != -1))
+        {
+            continue;
+        }
+
+        struct strbook* new_node = (struct strbook*)malloc(sizeof(struct strbook));
+        if (new_node == NULL)\
+        {
+            printf("分配内存失败。\n");
+            fclose(BOOK_FILE);
+            return head;
+        }
+
+        new_node->book_id = current_book_id;
+        new_node->time = current_time;
+        new_node->roomtype = current_roomtype;
+        new_node->last = current_last;
+        strncpy(new_node->id, current_id, sizeof(current_id));
+        new_node->status = current_status;
+
+        if(head == NULL)
+        {
+            head = new_node;
+            tail = new_node;
+        }
+
+        else
+        {
+            tail->next = new_node;
+            tail = new_node;
+        }
     }
+
+    fclose(BOOK_FILE);
+    return head;
 }
 
 // 统计所有预约
