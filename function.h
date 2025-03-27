c4789cf8170be4217e37914b1fa1c532dc00c00b
daisuki-noshiro <2311593398@qq.com>
2025-03-27 20:34:59 +0800

    Update function.h

--- function.h
+++ function.h
@@ -23,4 +23,11 @@ void freelist_room(struct room* head); //释放房间信息链表内存
 int change_room(struct room* head); //改变房间数据
 struct room* search_room(struct room* head, int num); //遍历链表查找ID对应结构体的函数，返回账户结构体地址
 void init_rooms(const char* roomdate); //初始化房间
-
+struct user* load_users(const char* users);  //将用户数据全部读取存入链表，返回首地址
+void write_users(const char* users, struct user* head); //将链表数据写入users文件中
+void freelist_user(struct user* head); //释放链表内存
+struct user* search_account(struct user* head, char ID[30]); //遍历链表查找ID对应结构体的函数，返回账户结构体地址 
+int change_password(struct user* head, char ID[30], char password[30]) //给客人和前台改密码的函数
+int search_card(struct user* head, char ID[30], char password[30]); //查询card值
+int change_card(struct user* head, char ID[30]); //管理员用修改等级
+int super_change_password(struct user* head, char ID[30]); //给管理员用的改密码函数，可以输入任意ID以更改其对应的密码 
