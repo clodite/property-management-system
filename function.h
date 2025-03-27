978415591cca4c09a2367b0e7df4eb49f017e3fd
szxzip-manjaro <64679700+szxzip@users.noreply.github.com>
2025-03-27 17:40:30 +0800

    'C'ontinue

--- function.h
+++ function.h
@@ -15,7 +15,7 @@ void menu_custom(char ID[30], char password[30], int type, char name[30], int ag
 
 int cancel_my_booking(long long my_book_id); // 取消自己的预约
 struct strbook* view_bookings(long long book_id, int time, int roomtype, int last, char id[30], int status);   // 按条件查询预约（-1不指定）
-under_construction sort_bookings(int type_x, int order_x, int type_y, int order_y); // 按条件排序预约：一二排序条件；增/减/不排序/1/-1/0
+struct strbook* sort_bookings(int type_x, int order_x, int type_y, int order_y); // 按条件排序预约：一二排序条件；增/减/不排序/1/-1/0
 int count_bookings(); // 统计预约（功能建设中）
 
 struct user* load_users(const char* users);  //将用户数据全部读取存入链表，返回首地址
