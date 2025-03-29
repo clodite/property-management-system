cab6ce8529281d8f9bcdd59fdac949ca7c9f7471
szxzip-manjaro <64679700+szxzip@users.noreply.github.com>
2025-03-29 14:31:43 +0800

    上传了一些shit

--- bookings_count.c
+++ bookings_count.c
@@ -6,7 +6,7 @@
 
 int bookings_count_times(long long book_id, int time, int roomtype, int last, char id[30], int status) // [预约] 次数统计：-1不指定
 {
-    return bookings_count_times_range(book_id, book_id, time, time, roomtype, roomtype, last, last, id[30], id[30], status);
+    return bookings_count_times_range(book_id, book_id, time, time, roomtype, roomtype, last, last, id, id, status);
 }
 
 int bookings_count_times_range(long long m_book_id, long long M_book_id, int m_time, int M_time, int m_roomtype, int M_roomtype, int m_last, int M_last, char m_id[30], char M_id[30], int status) // [预约] 范围次数统计：-1不指定
@@ -35,12 +35,17 @@ int bookings_count_times_range(long long m_book_id, long long M_book_id, int m_t
             continue;
         }
 
-        if ((current_book_id != book_id && book_id != -1) || (current_time != time && time != -1) || (current_roomtype != roomtype && roomtype != -1) || (current_last != last && last != -1) || (strcmp(current_id, id) && strcmp(id, "-1")) || (current_status != status && status != -1))
-        {
-            continue;
-        }
+        int m_id_strcmp_result = id_strcmp(current_id, m_id);
+        int M_id_strcmp_result = id_strcmp(current_id, M_id);
+        int m_strcmp_result = strcmp(m_id, "-1");
+        int M_strcmp_result = strcmp(M_id, "-1");
 
-        times++;
+        // WARNING: SHIT IS COOMING //
+
+        if (((current_book_id >= m_book_id && current_book_id <= M_book_id && m_book_id != -1 && M_book_id != -1) || (current_book_id >= m_book_id && m_book_id != -1 && M_book_id == -1) || (current_book_id <= M_book_id || M_book_id != -1 && m_book_id == -1) || (m_book_id == -1 && M_book_id == -1)) && ((current_time >= m_time && current_time <= M_time && m_time != -1 && M_time != -1) || (current_time >= m_time && m_time != -1 && M_time == -1) || (current_time <= M_time || M_time != -1 && m_time == -1) || (m_time == -1 && M_time == -1)) && ((current_roomtype >= m_roomtype && current_roomtype <= M_roomtype && m_roomtype != -1 && M_roomtype != -1) || (current_roomtype >= m_roomtype && m_roomtype != -1 && M_roomtype == -1) || (current_roomtype <= M_roomtype || M_roomtype != -1 && m_roomtype == -1) || (m_roomtype == -1 && M_roomtype == -1)) && ((current_last >= m_last && current_last <= M_last && m_last != -1 && M_last != -1) || (current_last >= m_last && m_last != -1 && M_last == -1) || (current_last <= M_last || M_last != -1 && m_last == -1) || (m_last == -1 && M_last == -1)) && ((m_id_strcmp_result != 1 && M_id_strcmp_result != -1 && m_strcmp_result != 0 && M_strcmp_result != 0) || (m_id_strcmp_result != 1 && m_strcmp_result != 0 && M_strcmp_result == 0) || (M_id_strcmp_result != -1 && M_strcmp_result != 0 && m_strcmp_result == 0) || (m_strcmp_result == 0 && M_strcmp_result == 0)) && (current_status == status || status == -1))
+        {
+            times++;
+        }
     }
 
     fclose(book_record);
@@ -49,11 +54,42 @@ int bookings_count_times_range(long long m_book_id, long long M_book_id, int m_t
 
 int bookings_count_guest(long long book_id, int time, int roomtype, int last, int status) // [预约] 人数统计：-1不指定
 {
-    return bookings_count_guestnum_range(book_id, book_id, time, time, roomtype, roomtype, lsat, last, status);
+    return bookings_count_guest_range(book_id, book_id, time, time, roomtype, roomtype, last, last, status);
 }
 
 int bookings_count_guest_range(long long m_book_id, long long M_book_id, int m_time, int M_time, int m_roomtype, int M_roomtype, int m_last, int M_last, int status) // [预约] 范围人数统计：-1不指定
 {
+    FILE* book_record;
+    book_record = fopen(BOOK_FILE, "r");
+    if (book_record == NULL)
+    {
+        printf("读取预约数据失败。\n");
+        return -1;
+    }
 
+    int times = 0;
+    char line[256];
+    int bug = 0; // 没用
+
+    while (fgets(line, sizeof(line), book_record))
+    {
+        long long current_book_id;
+        int current_time, current_roomtype, current_last, current_status;
+        char current_id [30];
+
+        if (sscanf(line, "%lld %d %d %d %c %d", &current_book_id, &current_time, &current_roomtype, &current_last, current_id, &current_status) != 6)
+        {
+            bug++;
+            continue;
+        }
+
+        if (((current_book_id >= m_book_id && current_book_id <= M_book_id && m_book_id != -1 && M_book_id != -1) || (current_book_id >= m_book_id && m_book_id != -1 && M_book_id == -1) || (current_book_id <= M_book_id || M_book_id != -1 && m_book_id == -1) || (m_book_id == -1 && M_book_id == -1)) && ((current_time >= m_time && current_time <= M_time && m_time != -1 && M_time != -1) || (current_time >= m_time && m_time != -1 && M_time == -1) || (current_time <= M_time || M_time != -1 && m_time == -1) || (m_time == -1 && M_time == -1)) && ((current_roomtype >= m_roomtype && current_roomtype <= M_roomtype && m_roomtype != -1 && M_roomtype != -1) || (current_roomtype >= m_roomtype && m_roomtype != -1 && M_roomtype == -1) || (current_roomtype <= M_roomtype || M_roomtype != -1 && m_roomtype == -1) || (m_roomtype == -1 && M_roomtype == -1)) && ((current_last >= m_last && current_last <= M_last && m_last != -1 && M_last != -1) || (current_last >= m_last && m_last != -1 && M_last == -1) || (current_last <= M_last || M_last != -1 && m_last == -1) || (m_last == -1 && M_last == -1)) && (current_status == status || status == -1))
+        {
+            times++;
+        }
+    }
+
+    fclose(book_record);
+    return times;
 }
 
